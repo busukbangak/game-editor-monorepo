99e7cbc8bf8434513f424273f10ae025f242541e
Aliim Wan <aliim.wanali@gmail.com>
2020-02-26 17:56:10 +0100

    Add basic layout

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 
+import { CoreModule } from '../core/core.module';
+import { SharedModule } from '../shared/shared.module';
+
+import { ShowroomComponent } from './showroom/showroom.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BottombarComponent } from './bottombar/bottombar.component';
+
 @NgModule({
-  declarations: [],
+  declarations: [
+    ShowroomComponent,
+    SidebarComponent,
+    BottombarComponent],
   imports: [
+    CoreModule,
+    SharedModule
+  ],
+  exports: [
+    ShowroomComponent,
+    SidebarComponent,
+    BottombarComponent
   ]
 })
 export class LayoutModule { }
