6cbd1cff86e59c0483e0f9c849913114711229e1
Aliim Wan <aliim.wanali@gmail.com>
2020-02-26 19:54:18 +0100

    Add threejs

--- src/app/layout/showroom/showroom.component.ts
+++ src/app/layout/showroom/showroom.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { EngineService } from '../../core/engine/engine.service';
 
 @Component({
   selector: 'app-showroom',
   templateUrl: './showroom.component.html',
   styleUrls: ['./showroom.component.scss']
 })
-export class ShowroomComponent implements OnInit {
 
-  constructor() { }
+export class ShowroomComponent implements OnInit, AfterViewInit {
+
+
+  @ViewChild('canvas', { static: true })
+  public canvas: ElementRef<HTMLCanvasElement>;
+
+  constructor(private engineService: EngineService) { }
 
   ngOnInit(): void {
+   
   }
+
+  ngAfterViewInit(): void {
+    this.engineService.createScene(this.canvas);
+    this.engineService.animate();
+  }
+
+
+
+
 
 }
