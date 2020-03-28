2e874e2a1f91b6fb2ef1e5252446df20f0ac3b8b
awanali <aliim.wanali@gmail.com>
2020-03-28 23:09:57 +0100

    Add script for transform controls

--- src/app/core/engine/engine.service.ts
+++ src/app/core/engine/engine.service.ts
@@ -10,8 +10,9 @@ import { Scene } from './.engine/components/Scene';
 import { Camera, CameraType } from './.engine/components/Camera';
 import { Light } from './.engine/components/Light';
 import { AssetManager } from './.engine/managers/AssetManager';
+
 import { OrbitControls } from './scripts/OrbitControls'
-import { Entity } from './.engine/entities/Entity';
+import { TransformControls } from './scripts/TransformControls'
 
 
 @Injectable({ providedIn: 'root' })
@@ -39,29 +40,36 @@ export class EngineService {
     let sceneComponent = this.app.world.createEntity()
       .addComponent(Scene, { active: true, background: new THREE.Color(0x959595) })
       .getComponent(Scene)
-      
+
     // create camera
     let cameraEntity = this.app.world.createEntity()
       .addComponent(Transform, { parent: sceneComponent })
       .addComponent(Camera, { active: true, type: CameraType.Perspective })
 
     cameraEntity.getComponent(Transform)
-      .value.position.z = 5;
-
-    cameraEntity.addComponent(Script, {
-      value: new OrbitControls(cameraEntity.getComponent(Camera).value, renderer.domElement)
-    });
+      .value.position.set(5, 5, 5);
 
     // create model
-    this.app.world.createEntity()
+    let modelEntity = this.app.world.createEntity()
       .addComponent(Transform, { parent: sceneComponent })
-      .addComponent(Model, { type: ModelType.Box, material: new THREE.MeshStandardMaterial({ color: 0xf28a3a, wireframe: true }) })
-      .addComponent(Script, { name: 'rotate' });
+      .addComponent(Model, { type: ModelType.Box, material: new THREE.MeshStandardMaterial({ color: 0xf28a3a, wireframe: false }) })
 
     // create light
     this.app.world.createEntity()
       .addComponent(Transform, { parent: sceneComponent })
       .addComponent(Light)
+
+    let orbit = new OrbitControls(cameraEntity.getComponent(Camera).value, renderer.domElement)
+
+
+    let transform = new TransformControls(cameraEntity.getComponent(Camera).value, renderer.domElement);
+    transform.addEventListener('dragging-changed', function (event) {
+      orbit.enabled = !event.value;
+    });
+
+    sceneComponent.value.add(transform)
+    transform.attach(modelEntity.getComponent(Model).value)
+
 
     this.app.start();
 
