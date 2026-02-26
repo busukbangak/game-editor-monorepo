5ae1cf38fce5eba1b8848d50809638d9be3335b7
awanali <aliim.wanali@gmail.com>
2026-02-26 11:31:42 +0100

    Add CommonModule and FormsModule imports to SharedModule for improved functionality

--- packages/game-editor/src/app/shared/shared.module.ts
+++ packages/game-editor/src/app/shared/shared.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [],
-  imports: [],
-  exports: []
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  exports: [
+    CommonModule,
+    FormsModule
+  ]
 })
 export class SharedModule {}
